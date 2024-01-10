c07c797bf06b9a677b8a394de3c4b28bb6a424b6
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-11 02:31:29 +0530

    chat

--- 07chat.js
+++ 07chat.js
@@ -1,7 +1,38 @@
 import {config} from "dotenv";
 config();
 
+import { ConverationChain } from "lamgchain/chains";
+import { ChatOpenAI } from "langchain/chat_models/openai";
 import {
-    chatPromptTemplate,
-    Hu
-}
+    ChatPromptTempelate,
+    humansMessagePromptTemplate,
+    SystemMessagePromptTempelate,
+    MessagePlaceholder,
+} from "langchain.prompts";
+import { BufferMemory } from "langchain/memory";
+
+const chat = new ChatOpenAI({ temperature: O });
+
+const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+  SystemMessagePromptTemplate.fromTemplate(
+    "The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know."
+  ),
+  new MessagesPlaceholder("history"),
+  HumanMessagePromptTemplate.fromTemplate("{input}"),
+]);
+
+const chain = new ConversationChain({
+    memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
+    prompt: chatPrompt,
+    llm: chat,
+});
+
+const response = await chain.call({
+    input: "What is the capital of France?",
+});
+
+const response2 = await chain.call({
+    input: "What is a great place to see there?",
+});
+
+console.log(response2);
